84a58940b6cda30e9f8fcf9867f8e15e0beb82d1
g g <gabedotgutierrez@gmail.com>
2024-11-12 18:35:58 -0500

    second model made

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
+PROC = (
+    ('UPL', 'Upload'),
+    ('DEV', 'Develop'),
+    ('SCN', 'Scan'),
+    ('PRT','Print')
+)
 
 class Camera(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +14,20 @@ class Camera(models.Model):
     format = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     year_made = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Upload(models.Model):
+    date = models.DateField()
+    process = models.CharField(
+        max_length=3,
+        choices = PROC,
+        default = PROC[0][0]
+    )
+
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
+
+    def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_process_display()} on {self.date}"
--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Camera
+from .models import Camera, Upload
 
 class CameraSerializer(serializers.ModelSerializer):
     class Meta:
         model = Camera
         fields = '__all__'
+
+class UploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Upload
+        fields = '__all__'
+        read_only_fields = ('camera',)
